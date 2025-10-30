b6c75c05a8ea6bbdafa3723ba76bebcf777f3b89
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-30 13:44:27 +0900

    auto commit: 2025-10-30 04:44:27

--- template/under.php
+++ template/under.php
@@ -1,7 +1,7 @@
 <div class="under_title">
     <div class="under_title_inner center">
         <h2>
-            <?php if(): ?>
+            <?php if(is_page('about')): ?>
                 <img class="sp contents_title" src="<?php echo get_template_directory_uri(); ?>/img/title_about_sp.svg" alt="アバウト">
                 <img class="pc contents_title" src="<?php echo get_template_directory_uri(); ?>/img/title_about_pc.svg" alt="アバウト">
             <?php endif; ?>
