716dd2f3f5d40954aafeb6c26fe51aa1f3515401
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-30 13:06:42 +0900

    auto commit: 2025-10-30 04:06:41

--- page.php
+++ page.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 <?php get_template_part('template/under'); ?>
 
-    
+<?php if(have_posts()): ?>
+    <?php while(have_posts()):the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+<?php endif; ?>
 
 
 
