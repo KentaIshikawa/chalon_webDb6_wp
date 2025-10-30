55159f5cd4dfb331363b2648974715354e84dfef
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-30 13:04:46 +0900

    auto commit: 2025-10-30 04:04:46

--- page.php
+++ page.php
@@ -1,52 +1,7 @@
 <?php get_header(); ?>
 <?php get_template_part('template/under'); ?>
 
-    <section class="about">
-        <h3 class="page_title">当店のこだわり</h3>
-        <section class="about_sozai">
-            <div class="about_sozai_inner center">
-                <h4 class="section_title">最高の素材を・・・</h4>
-                <div class="about_sozai_text">
-                    <p>ケーキ作りに使う素材は、店主自ら各地に足を運んで、一つ一つしっかりと選んでいます。</p>
-                    <p>自然に近い形で飼育された鶏から、その日にとれた新鮮な卵を埼玉の養鶏場から。</p>
-                    <p>生クリームは厳しい寒さの中で強く育った牛から取れる、北海道産のコクのある物を。</p>
-                    <p>バターは無農薬牧草で育った乳牛の風味とコクのある最高品質のバターを。</p>
-                    <p>フルーツは旬の味覚を大事にし、農薬を最小限に抑えたなるべく安全な物を。</p>
-                    <p>小麦は、数ある小麦粉の中から選び抜いた、香りの良い北海道産の「ファリーヌ」にこだわっています。</p>
-                    <p>店主のこだわりが詰まったCHALONのケーキを、是非召し上がってください。</p>
-                </div>
-                <div class="about_sozai_img img_wrap">
-                    <picture class="to_right">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/about_01.jpg" alt="卵の写真">
-                    </picture>
-                    <picture class="to_left">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/about_02.jpg" alt="小麦の写真">
-                    </picture>
-                </div>
-            </div>
-        </section>
-
-        <section class="about_kuukan">
-            <div class="about_kuukan_inner center">
-                <h4 class="section_title">落ち着ける空間を・・・</h4>
-                <div class="about_kuukan_text">
-                    <p>CHALONでは、買ったその場でケーキを召し上がっていただけるよう、イートインスペースを設けています。是非ご利用ください。</p>
-                    <p>イートインスペースの椅子やテーブルは、温かみのある北欧家具の物を選んでいます。</p>
-                    <p>また、食器も海外のものを取り寄せています。</p>
-                    <p>照明は明るすぎず、読書や会話を邪魔しない、優しい明るさにしています。</p>
-                    <p>コーヒーや紅茶と一緒に、ティータイムを楽しんで行って下さい。</p>
-                </div>
-                <div class="about_kuukan_img img_wrap">
-                    <picture class="to_right">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/about_03.jpg" alt="店内の写真">
-                    </picture>
-                    <picture class="to_left">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/about_04.jpg" alt="テーブルの写真">
-                    </picture>
-                </div>
-            </div>
-        </section>
-    </section>
+    
 
 
 
