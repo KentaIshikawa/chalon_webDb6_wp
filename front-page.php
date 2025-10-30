9408a15d0a26c7e38aebab4f05899c163795b5f0
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-30 09:26:22 +0900

    auto commit: 2025-10-30 00:26:22

--- front-page.php
+++ front-page.php
@@ -217,24 +217,3 @@
     </div>
 
 
-    <footer>
-        <div class="footer_inner">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo_white.svg" alt="洋菓子店CHALON">
-            <address>
-                TEL:03-1234-5678<br>
-                〒132-4567 東京都新宿区愛住町 22 第三山田ビル1F
-            </address>
-            <small>&copy;CHALON</small>
-        </div>
-    </footer>
-
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <!-- slickのJavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/common.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/top.js"></script>
-</body>
-
-</html>
