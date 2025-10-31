ed303b8e719e6a59a0e2abfe1e566ea3070a0576
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-31 11:10:35 +0900

    auto commit: 2025-10-31 02:10:35

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php get_header(); ?>
 <?php get_template_part('template/under'); ?>
+<?php if(have_posts()): ?>
+    <?php while(have_posts()):the_post(); ?>
+        <section class="news">
+            <h3 class="page_title"><?php the_title(); ?></h3>
+            <time class="news_time" datetime="2023-08-07">2023-08-07</time>
+            <div class="news_inner">
+                <div class="news_contents">
 
-    <section class="news">
-        <h3 class="page_title">お知らせタイトル</h3>
-        <time class="news_time" datetime="2023-08-07">2023-08-07</time>
-        <div class="news_inner">
-            <div class="news_contents">
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
-                <p>お知らせテキスト</p>
+                </div>
+                <a href="../" class="btn">TOPへ戻る</a>
             </div>
-            <a href="../" class="btn">TOPへ戻る</a>
-        </div>
-    </section>
-
+        </section>
+    <?php endwhile; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
