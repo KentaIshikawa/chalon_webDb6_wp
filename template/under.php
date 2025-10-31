e9a905987e64838db3f377dd952122078053af87
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-31 11:07:10 +0900

    auto commit: 2025-10-31 02:07:09

--- template/under.php
+++ template/under.php
@@ -16,6 +16,10 @@
                     $file = 'trial';
                     $alt = '無料体験お申し込み';
                 }
+                if(is_singular('post')){
+                    $file = 'news';
+                    $alt = 'お知らせ';
+                }
             ?>
             <img class="sp contents_title" src="<?php echo get_template_directory_uri(); ?>/img/title_<?php echo $file; ?>_sp.svg" alt="<?php echo $alt; ?>">
             <img class="pc contents_title" src="<?php echo get_template_directory_uri(); ?>/img/title_<?php echo $file; ?>_pc.svg" alt="<?php echo $alt; ?>">
