b16b9b3184cca5071406230fcea8ab5eabae9b41
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-31 13:21:04 +0900

    auto commit: 2025-10-31 04:21:04

--- single-menu.php
+++ single-menu.php
@@ -3,34 +3,32 @@
 
 <?php if(have_posts()): ?>
     <?php while(have_posts()):the_post(); ?>
-        
-
-    <section class="menu_single">
-        <div class="menu_single_inner">
-            <h3 class="page_title">ショートケーキ</h3>
-            <div class="menu_single_box">
-                <picture>
-                    <source srcset="../../img/menu_01.webp" type="image/webp">
-                    <img src="../../img/menu_01.jpg" alt="ショートケーキ">
-                </picture>
-                <div>
-                    <dl>
-                        <dt>価格</dt>
-                        <dd>600円</dd>
-                    </dl>
-                    <dl>
-                        <dt>カロリー</dt>
-                        <dd>343kcal</dd>
-                    </dl>
-                    <dl>
-                        <dt>アレルギー</dt>
-                        <dd>卵、乳、小麦</dd>
-                    </dl>
+        <section class="menu_single">
+            <div class="menu_single_inner">
+                <h3 class="page_title"><?php the_title(); ?></h3>
+                <div class="menu_single_box">
+                    <picture>
+                        <source srcset="../../img/menu_01.webp" type="image/webp">
+                        <img src="../../img/menu_01.jpg" alt="ショートケーキ">
+                    </picture>
+                    <div>
+                        <dl>
+                            <dt>価格</dt>
+                            <dd>600円</dd>
+                        </dl>
+                        <dl>
+                            <dt>カロリー</dt>
+                            <dd>343kcal</dd>
+                        </dl>
+                        <dl>
+                            <dt>アレルギー</dt>
+                            <dd>卵、乳、小麦</dd>
+                        </dl>
+                    </div>
                 </div>
+                <a href="../" class="btn">メニュー一覧へ</a>
             </div>
-            <a href="../" class="btn">メニュー一覧へ</a>
-        </div>
-    </section>
+        </section>
     <?php endwhile; ?>
 <?php endif; ?>
 
