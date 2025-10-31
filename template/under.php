a69b868c23f9027414f37314ed2b844c2aac0c28
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-31 09:15:44 +0900

    auto commit: 2025-10-31 00:15:44

--- template/under.php
+++ template/under.php
@@ -12,6 +12,9 @@
                     $file = 'access';
                     $alt = 'アクセス';
                 }
+                if(is_page(['trial','check','thanks'])){
+
+                }
             ?>
             <img class="sp contents_title" src="<?php echo get_template_directory_uri(); ?>/img/title_<?php echo $file; ?>_sp.svg" alt="<?php echo $alt; ?>">
             <img class="pc contents_title" src="<?php echo get_template_directory_uri(); ?>/img/title_<?php echo $file; ?>_pc.svg" alt="<?php echo $alt; ?>">
