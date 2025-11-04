93134fa897b40dd2060b66f10ff88e27467db047
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-11-04 13:19:33 +0900

    auto commit: 2025-11-04 04:19:33

--- page-lesson.php
+++ page-lesson.php
@@ -93,16 +93,14 @@
                                     <?php echo SCF::get('age'); ?><?php echo SCF::get('seibetsu'); ?>
                                     <?php if(SCF::get('child_sanka')==='有'): ?>
                                         <br>
-                                        お子さん2人と参加
+                                        <?php
+                                            $children = SCF::get('children');
+                                        ?>
+                                        お子さん<?php echo count($children); ?>人と参加
                                     <?php endif; ?>
                                 </p>
                                 <p class="lesson_voice_box_text">
-                                   <?php
-                                    $children = SCF::get('children');
-                                    echo '<pre>';
-                                    print_r(count($children));
-                                    echo '</pre>';
-                                   ?>
+                                   
                                 </p>
                                 
                                 <a href="voice/"></a>
