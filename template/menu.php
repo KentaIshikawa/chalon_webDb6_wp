5e0738595cae97298b2ba3ae26a36e7776fd407a
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-11-04 09:42:02 +0900

    auto commit: 2025-11-04 00:42:02

---
+++ template/menu.php
@@ -0,0 +1,7 @@
+<figure>
+    <picture>
+        <?php get_template_part('template/thumbnail'); ?>
+    </picture>
+    <figcaption><?php the_title(); ?></figcaption>
+    <a href="<?php the_permalink(); ?>"></a>
+</figure>
