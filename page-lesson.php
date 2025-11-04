2f2b3148be4e9200ad704c1d47bba7783a433400
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-11-04 12:57:05 +0900

    auto commit: 2025-11-04 03:57:04

--- page-lesson.php
+++ page-lesson.php
@@ -85,7 +85,7 @@
                                 <picture>
                                     <?php
                                     $img = SCF::get('archive_img');
-                                    echo wp_get_attachment_url($img);
+                                    
                                     ?>
                                 </picture>
                                 <p class="lesson_voice_box_title">20代女性 お子さん2人と参加</p>
@@ -93,6 +93,7 @@
                                     子供二人と、三人で参加しました。一つ一つの工程がとてもわかりやすかったです。<br>
                                     家でも作りやすい内容だったので、オーブンを買って子供と一緒に挑戦しようと思っています。
                                 </p>
+                                <?php echo wp_get_attachment_url($img); ?>
                                 <a href="voice/"></a>
                             </div>
                         <?php endwhile; ?>
