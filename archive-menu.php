e10cbc63695c6829f35510a050d9d726c34379e3
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-11-04 09:09:25 +0900

    auto commit: 2025-11-04 00:09:25

--- archive-menu.php
+++ archive-menu.php
@@ -1,48 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>洋菓子店CHALON</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Serif+JP:wght@400;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <!-- slickのCSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css">
-    <link rel="stylesheet" href="../css/common/reset.css">
-    <link rel="stylesheet" href="../css/common/common.css">
-    <link rel="stylesheet" href="../css/common/under.css">
-    <link rel="stylesheet" href="../css/page/menu.css">
-</head>
-
-<body>
-    <header>
-        <div class="header_inner">
-            <h1 class="logo">
-                <a href="../">
-                    <img src="../img/logo.svg" alt="洋菓子店CHALON">
-                </a>
-            </h1>
-            <div class="hum_menu">
-                <span class="hum_menu_line"></span>
-                <span class="hum_menu_text">MENU</span>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="../about/">当店について</a></li>
-                    <li><a href="./">メニュー</a></li>
-                    <li><a href="../lesson/">お菓子教室</a></li>
-                    <li><a href="../access/">アクセス</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+<?php get_header(); ?>
     <div class="under_title">
         <div class="under_title_inner center">
             <h2>
