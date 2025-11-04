506ebcb66b96a716502b0bf08d7a27be2ee1e3d5
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-11-04 09:06:54 +0900

    auto commit: 2025-11-04 00:06:54

---
+++ archive-menu.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>洋菓子店CHALON</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Serif+JP:wght@400;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <!-- slickのCSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css">
+    <link rel="stylesheet" href="../css/common/reset.css">
+    <link rel="stylesheet" href="../css/common/common.css">
+    <link rel="stylesheet" href="../css/common/under.css">
+    <link rel="stylesheet" href="../css/page/menu.css">
+</head>
+
+<body>
+    <header>
+        <div class="header_inner">
+            <h1 class="logo">
+                <a href="../">
+                    <img src="../img/logo.svg" alt="洋菓子店CHALON">
+                </a>
+            </h1>
+            <div class="hum_menu">
+                <span class="hum_menu_line"></span>
+                <span class="hum_menu_text">MENU</span>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="../about/">当店について</a></li>
+                    <li><a href="./">メニュー</a></li>
+                    <li><a href="../lesson/">お菓子教室</a></li>
+                    <li><a href="../access/">アクセス</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="under_title">
+        <div class="under_title_inner center">
+            <h2>
+                <img class="sp contents_title" src="../img/title_menu_sp.svg" alt="メニュー">
+                <img class="pc contents_title" src="../img/title_menu_pc.svg" alt="メニュー">
+            </h2>
+        </div>
+    </div>
+    <section class="menu">
+        <h3 class="page_title">メニュー一覧</h3>
+        <section class="menu_cake">
+            <div class="menu_cake_inner center">
+                <h4 class="section_title">ケーキ</h4>
+                <div class="menu_cake_text">
+                    <p>CHALONのケーキは、国産の美味しくて安全な食材を使っています。スポンジケーキには、その日の朝にとれた卵を使っているので、卵の味が優しくしっかりしています。また、記念日のケーキなどもお受けしていますので店員にご相談ください。
+                    </p>
+                </div>
+                <div class="menu_cake_img">
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_01.webp" type="image/webp">
+                            <img src="../img/menu_01.jpg" alt="ショートケーキ">
+                        </picture>
+                        <figcaption>ショートケーキ</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_02.webp" type="image/webp">
+                            <img src="../img/menu_02.jpg" alt="チョコレートケーキ">
+                        </picture>
+                        <figcaption>チョコレートケーキ</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_03.webp" type="image/webp">
+                            <img src="../img/menu_03.jpg" alt="チーズケーキ">
+                        </picture>
+                        <figcaption>チーズケーキ</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_04.webp" type="image/webp">
+                            <img src="../img/menu_04.jpg" alt="ベイクドチーズケーキ">
+                        </picture>
+                        <figcaption>ベイクドチーズケーキ</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_05.webp" type="image/webp">
+                            <img src="../img/menu_05.jpg" alt="モンブラン">
+                        </picture>
+                        <figcaption>モンブラン</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_06.webp" type="image/webp">
+                            <img src="../img/menu_06.jpg" alt="ロールケーキ">
+                        </picture>
+                        <figcaption>ロールケーキ</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_07.webp" type="image/webp">
+                            <img src="../img/menu_07.jpg" alt="フルーツタルト">
+                        </picture>
+                        <figcaption>フルーツタルト</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_08.webp" type="image/webp">
+                            <img src="../img/menu_08.jpg" alt="イチゴのタルト">
+                        </picture>
+                        <figcaption>イチゴのタルト</figcaption>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_09.webp" type="image/webp">
+                            <img src="../img/menu_09.jpg" alt="ティラミス">
+                        </picture>
+                        <figcaption>ティラミス</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </section>
+
+        <section class="menu_other">
+            <div class="menu_other_inner center">
+                <h4 class="section_title">焼き菓子・その他</h4>
+                <div class="menu_other_text">
+                    <p>ケーキ以外にも、クッキーやマカロン、ゼリーなどもご用意しております。一つ一つ個包装になっているので、お友達などとシェアしやすくなっています。焼き菓子といえど、食材の新鮮さにはこだわっています。
+                    </p>
+                </div>
+                <div class="menu_other_img">
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_10.webp" type="image/webp">
+                            <img src="../img/menu_10.jpg" alt="バタークッキー">
+                        </picture>
+                        <figcaption>バタークッキー</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_11.webp" type="image/webp">
+                            <img src="../img/menu_11.jpg" alt="チョコチップクッキー">
+                        </picture>
+                        <figcaption>チョコチップクッキー</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_12.webp" type="image/webp">
+                            <img src="../img/menu_12.jpg" alt="クッキーセット">
+                        </picture>
+                        <figcaption>クッキーセット</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_13.webp" type="image/webp">
+                            <img src="../img/menu_13.jpg" alt="フルーツのゼリー">
+                        </picture>
+                        <figcaption>フルーツのゼリー</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_14.webp" type="image/webp">
+                            <img src="../img/menu_14.jpg" alt="マカロン">
+                        </picture>
+                        <figcaption>マカロン</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <section class="menu_drink">
+            <div class="menu_drink_inner center">
+                <h4 class="section_title">ドリンク</h4>
+                <div class="menu_drink_text">
+                    <p>ケーキをなるべく早く、美味しく召し上がって頂く為にイートインスペースを作りました。イートインスペースでのみ、ドリンクをご用意しています。ケーキと一緒に是非召し上がってください。</p>
+                </div>
+                <div class="menu_drink_img">
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_15.webp" type="image/webp">
+                            <img src="../img/menu_15.jpg" alt="コーヒー">
+                        </picture>
+                        <figcaption>コーヒー</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_16.webp" type="image/webp">
+                            <img src="../img/menu_16.jpg" alt="アイスコーヒー">
+                        </picture>
+                        <figcaption>アイスコーヒー</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_17.webp" type="image/webp">
+                            <img src="../img/menu_17.jpg" alt="紅茶">
+                        </picture>
+                        <figcaption>紅茶</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                    <figure>
+                        <picture>
+                            <source srcset="../img/menu_18.webp" type="image/webp">
+                            <img src="../img/menu_18.jpg" alt="アイスティー">
+                        </picture>
+                        <figcaption>アイスティー</figcaption>
+                        <a href="#"></a>
+                    </figure>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <footer>
+        <div class="footer_inner">
+            <img src="../img/logo_white.svg" alt="洋菓子店CHALON">
+            <address>
+                TEL:03-1234-5678<br>
+                〒132-4567 東京都新宿区愛住町 22 第三山田ビル1F
+            </address>
+            <small>&copy;CHALON</small>
+        </div>
+    </footer>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="../js/common.js"></script>
+</body>
+
+</html>
